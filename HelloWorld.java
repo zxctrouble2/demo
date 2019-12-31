0d7064a6ac2f9e3a07b1c091584478c95766cde2
Dz_git <=2541714676@qq.com>
2019-12-31 19:39:04 +0800

    hello world

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class Hello World{
+	public static void main(String[] args){
+	System.out.println("1314");
+System.out.println("Lihao");
+System.out.println("Lihao");
+System.out.println("520");
+}
+}
