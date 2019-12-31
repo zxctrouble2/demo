8f48f88805e74b71322514976de39c7382b53924
Dz_git <=2541714676@qq.com>
2019-12-31 17:38:56 +0800

    java commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class Hello World{
+	public static void main(String[] args){
+	System.out.println("Lihao");
+System.out.println("Lihao");
+System.out.println("Lihao");
+System.out.println("Lihao");
+}
+}
